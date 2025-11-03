ecb758f82cdbae88769535fd85f9a8092b74b205
github-ci <you@example.com>
2025-11-03 14:29:06 +0800

    refactor(user-agreement-bundle): 解耦 BizUserBundle，使用 user-service-contracts 接口

--- src/YouzanApiBundle.php
+++ src/YouzanApiBundle.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class YouzanApiBundle extends Bundle implements BundleDependencyInterface
 {
@@ -16,6 +17,7 @@ class YouzanApiBundle extends Bundle implements BundleDependencyInterface
             DoctrineBundle::class => ['all' => true],
             TwigBundle::class => ['all' => true],
             EasyAdminBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
